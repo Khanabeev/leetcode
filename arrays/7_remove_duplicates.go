91354321e1ceb7e1b78067ab9392f8ba9fdcc3e5
Антон Ханабеев <p8@iqvector.ru>
2021-03-22 08:38:28 +0300

    class 7

--- arrays/7_remove_duplicates.go
+++ arrays/7_remove_duplicates.go
@@ -1,21 +1,44 @@
 package arrays
 
-import "sort"
+import "fmt"
 
 func removeDuplicates(nums []int) int {
-	sort.Ints(nums)
-	return len(unique(nums))
+	n, d := RemoveDuplicates(nums)
+	fmt.Println(d)
+	return len(n)
 }
 
-func unique(intSlice []int) []int {
-	keys := make(map[int]bool)
-	list := []int{}
-
-	for _, entry := range intSlice {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
+func RemoveDuplicates(s []int) ([]int, []int) {
+	encountered := make(map[int]struct{})
+	result := make([]int, 0)
+	duplicate := make([]int, 0)
+	for _, v := range s {
+		if _, ok := encountered[v]; ok {
+			duplicate = append(duplicate, v)
+			continue
+		} else {
+			encountered[v] = struct{}{}
+			result = append(result, v)
 		}
 	}
-	return list
+	return result, duplicate
+}
+
+func RemoveDuplicates2(elements []int) []int {
+	// Use map to record duplicates as we find them.
+	encountered := map[int]bool{}
+	result := []int{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+			// Append to result slice.
+			result = append(result, elements[v])
+		}
+	}
+	// Return the new slice.
+	return result
 }
